decf96dd21f5265830a0cd6259360584e8857a80
Max <max.pewny@gmail.com>
2020-02-17 20:45:18 +0100

    Circular Buffer passes given Test
    Optimization and Comments required

--- CircularBuffer/CircularBuffer.cs
+++ CircularBuffer/CircularBuffer.cs
@@ -1,23 +1,25 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Text;
+
 
 namespace CircularBuffer
 {
     class CircularBuffer<T> : ICircularBuffer<T>
     {
         public int Capacity => mArray.Length;
-        public int Count => mCounter;
-        public bool IsEmpty => mCounter == 0;
-        public bool IsFull => mCounter == mArray.Length;
+        public int Count { get; private set; } = 0;
+        public bool IsEmpty => Count == 0;
+        public bool IsFull => Count == mArray.Length;
 
         private T[] mArray;
-        private int mCounter = 0;
+        private object mutex = new object();
 
         public CircularBuffer(int pSize)
         {
-            mArray = new T[pSize];
+            lock (mutex)
+            {
+                mArray = new T[pSize];
+            }
         }
         
         public void Produce(T pT)
@@ -26,9 +28,12 @@ namespace CircularBuffer
             {
                 throw new BufferOverflowException("BUFFER_OVERFLOW: can't add Value because the Buffer is full");
             }
+            lock (mutex)
+            {
+                mArray[Count] = pT;
+                ++Count;
+            }
             
-            mArray[mCounter] = pT;
-            ++mCounter;
         }
 
         public int ProduceAll<T2>(IEnumerable<T2> pEnumT) where T2 : T
@@ -58,15 +63,16 @@ namespace CircularBuffer
             {
                 throw new BufferUnderflowException("BUFFER_UNDERFLOW: can't consume from empty Buffer");
             }
-
-            T t = mArray[0];
-            for (int i = 1; i < mCounter; i++)
+            lock (mutex)
             {
-                mArray[i - 1] = mArray[i];
+                T t = mArray[0];
+                for (int i = 1; i < Count; i++)
+                {
+                    mArray[i - 1] = mArray[i];
+                }
+                --Count;
+                return t;
             }
-            --mCounter;
-            return t;
-
         }
 
         public void ConsumeAll(Action<T> pAct)
@@ -76,15 +82,19 @@ namespace CircularBuffer
                 throw new BufferUnderflowException("BUFFER_UNDERFLOW: can't consume from empty Buffer");
             }
 
-            for (int i = 0; i < mCounter; i++)
+            for (int i = 0; i < Count; i++)
             {
                 pAct(Consume());
+            }
+            if (!IsEmpty)
+            {
+                ConsumeAll(pAct);
             }
         }
 
         public void Clear()
         {
-            mCounter = 0;
+            Count = 0;
         }
     }
 }
